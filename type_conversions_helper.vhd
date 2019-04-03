b9cc4b4e6d27e3d8eaf97751b0a4b0d9ba6cd52c
rpeschke <peschke@hawaii.edu>
2019-04-03 14:03:41 +0900

    addd conversion from sl to slv and non conversion from slv to slv

--- type_conversions_helper.vhd
+++ type_conversions_helper.vhd
@@ -5,6 +5,10 @@ use ieee.numeric_std.all;
 package type_conversions_pgk is
 
 
+  
+ procedure slv_to_slv(signal  SLV_in : in STD_LOGIC_VECTOR ; signal SLV_out : out STD_LOGIC_VECTOR);
+ procedure slv_to_sl(signal  SLV_in : in STD_LOGIC_VECTOR ; signal SL_out : out STD_LOGIC);
+ procedure sl_to_slv(signal  SL_in : in STD_LOGIC ; signal SL_out : out STD_LOGIC_VECTOR);
 procedure integer_to_sl(signal I_in : in integer; signal SL_out : out STD_LOGIC);
 procedure integer_to_slv(signal I_in : in integer; signal SLV_out : out STD_LOGIC_VECTOR);                                                                        
 procedure integer_to_slv_var(signal I_in : in integer; SLV_out : out STD_LOGIC_VECTOR);   
@@ -20,6 +24,17 @@ procedure sl_to_integer(signal SL_in : in STD_LOGIC ; signal Int_out : out integ
 end type_conversions_pgk;
 
 package body type_conversions_pgk is
+   
+  procedure sl_to_slv(signal  SL_in : in STD_LOGIC ; signal SL_out : out STD_LOGIC_VECTOR) is begin 
+     SL_out(0) <= SL_in;
+  end procedure;
+   
+ procedure slv_to_sl(signal  SLV_in : in STD_LOGIC_VECTOR ; signal SL_out : out STD_LOGIC) is begin 
+   SL_out <= SLV_in(0);
+ end procedure;
+  procedure slv_to_slv(signal  SLV_in : in STD_LOGIC_VECTOR ; signal SLV_out : out STD_LOGIC_VECTOR) is begin
+    SLV_out <= SLV_in;
+  end procedure;
   
 procedure natural_to_integer(signal Nat_in :in natural ; signal int_out :out integer) is begin 
   
